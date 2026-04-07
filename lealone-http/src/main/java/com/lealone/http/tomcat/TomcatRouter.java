5afccc67d24ee1cbde57b7cb82afa70d6acf0996
Honghua Zhu <zhh200910@gmail.com>
2026-04-07 20:58:46 +0800

    增加TemplateFilter

--- lealone-http/src/main/java/com/lealone/http/tomcat/TomcatRouter.java
+++ lealone-http/src/main/java/com/lealone/http/tomcat/TomcatRouter.java
@@ -15,7 +15,9 @@ import org.apache.tomcat.util.descriptor.web.FilterMap;
 import com.lealone.service.ServiceHandler;
 import com.lealone.service.http.HttpRouter;
 import com.lealone.service.http.HttpServer;
+import com.lealone.service.template.TemplateEngine;
 
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.Servlet;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
@@ -25,16 +27,21 @@ import jakarta.servlet.http.HttpServletResponse;
 public class TomcatRouter implements HttpRouter {
 
     protected TomcatServer tomcatServer;
+    protected String webRoot;
 
     @Override
     public void init(HttpServer server, Map<String, String> config) {
         tomcatServer = (TomcatServer) server;
+        webRoot = config.get("web_root");
         init(config);
     }
 
     public void init(Map<String, String> config) {
         addServlet("serviceServlet", new TomcatServiceServlet(new ServiceHandler(config)), "/service/*");
         addServlet("defaultServlet", new TomcatDefaultServlet(config), "/");
+        if (isDevelopmentEnvironment(config)) {
+            addFilter(new TemplateFilter(), "*.html");
+        }
         tomcatServer.getContext().addWelcomeFile("index.html");
     }
 
@@ -60,6 +67,33 @@ public class TomcatRouter implements HttpRouter {
         map.addURLPattern(urlPattern);
         tomcatServer.getContext().addFilterDef(def);
         tomcatServer.getContext().addFilterMap(map);
+    }
+
+    private boolean isDevelopmentEnvironment(Map<String, String> config) {
+        String environment = config.get("environment");
+        if (environment != null) {
+            environment = environment.trim().toLowerCase();
+            if (environment.equals("development") || environment.equals("dev")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private class TemplateFilter extends HttpFilter {
+        @Override
+        protected void doFilter(HttpServletRequest request, HttpServletResponse response,
+                FilterChain chain) throws IOException, ServletException {
+            TemplateEngine te = new TemplateEngine(webRoot, "utf-8");
+            String file = request.getRequestURI();
+            try {
+                String str = te.process(file);
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("text/html; charset=utf-8");
+                response.getWriter().write(str);
+            } catch (Exception e) {
+            }
+        }
     }
 
     private static class TomcatDefaultServlet extends DefaultServlet {
